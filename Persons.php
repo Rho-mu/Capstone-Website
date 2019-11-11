4a3282ac051034744a7d77b35b1688f49dab49e6
Haitian11 <47162113+Haitian11@users.noreply.github.com>
2019-11-11 14:07:38 -0700

    Add files via upload

---
+++ Persons.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.8.4
+ */
+
+/**
+ * Database `capstone demo`
+ */
+
+/* `capstone demo`.`Persons` */
+$Persons = array(
+  array('LastName' => 'hai','FirstName' => 'tian','Location' => 'Flagstaff','Affiliation' => 'college')
+);
